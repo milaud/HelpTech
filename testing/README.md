68f56357c2f41fa81a7cc4a356d5e4d540b0a1cb
Michelle <laimich3@gmail.com>
2017-03-27 00:43:55 -0700

    Adding project files

---
+++ testing/README.md
@@ -0,0 +1,1 @@
+# HAMML-Tech
---
+++ testing/app/src/main/java/hammltechnology/helptech/MainScreen.java
@@ -0,0 +1,16 @@
+package hammltechnology.helptech;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Michelle on 3/27/2017.
+ */
+
+public class MainScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+    }
+}
