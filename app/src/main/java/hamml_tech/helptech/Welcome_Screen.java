f3422f3f0e3f9440a78270e9551efb801996e2b2
milaud <nmilaud@gmail.com>
2017-03-27 21:07:05 -0700

    Added home page.

---
+++ app/src/main/java/hamml_tech/helptech/Welcome_Screen.java
@@ -0,0 +1,13 @@
+package hamml_tech.helptech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome__screen);
+    }
+}
