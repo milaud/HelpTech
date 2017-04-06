6bd1884754a5247cc134e5aff0e94c7109d577d1
milaud <nmilaud@gmail.com>
2017-04-06 00:35:21 -0700

    Added View Tutorials page and linked it from Welcome Screen

--- app/src/main/java/hamml_tech/helptech/Welcome_Screen.java
+++ app/src/main/java/hamml_tech/helptech/Welcome_Screen.java
@@ -1,7 +1,10 @@
 package hamml_tech.helptech;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome_Screen extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Welcome_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome__screen);
+
+        //view tutorials button
+        final Button button = (Button) findViewById(R.id.view_tutorials);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Welcome_Screen.this, view_tutorials_1.class));
+            }
+        });
     }
 }
