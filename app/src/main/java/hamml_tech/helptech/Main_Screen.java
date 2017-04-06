036b96817d1adbc983a9924ad1f1bec33994a3d0
milaud <nmilaud@gmail.com>
2017-04-06 01:29:05 -0700

    Updated Main Screen and Step by Step Guides.

--- app/src/main/java/hamml_tech/helptech/Main_Screen.java
+++ app/src/main/java/hamml_tech/helptech/Main_Screen.java
@@ -1,8 +1,8 @@
 package hamml_tech.helptech;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -24,5 +24,16 @@ public class Main_Screen extends AppCompatActivity {
                 println("test");
             }
         });
+
+        //step by step guides button
+        Button step = (Button)findViewById(R.id.button3);
+        ImageButton image3 = (ImageButton)findViewById(R.id.imageButton3);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main_Screen.this, step_guides_1.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/hamml_tech/helptech/step_guides_1.java
+++ app/src/main/java/hamml_tech/helptech/step_guides_1.java
@@ -3,11 +3,11 @@ package hamml_tech.helptech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class view_tutorials_1 extends AppCompatActivity {
+public class step_guides_1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_tutorials_1);
+        setContentView(R.layout.activity_step_guides_1);
     }
 }
