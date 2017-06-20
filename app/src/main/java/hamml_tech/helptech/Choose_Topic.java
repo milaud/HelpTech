9c3c01be7b5db3359fa557465f5bf22cd03b38da
Michelle <laimich3@gmail.com>
2017-06-20 13:41:48 -0700

    Fixed build problems

--- app/src/main/java/hamml_tech/helptech/Choose_Topic.java
+++ app/src/main/java/hamml_tech/helptech/Choose_Topic.java
@@ -8,6 +8,6 @@ public class Choose_Topic extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_topic);
+        //setContentView(R.layout.activity_choose_topic);
     }
 }
